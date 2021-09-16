6451e035189695db00b3c236f6fe45d1c86efad3
shyamprasadc <shyamprasadc1995@gmail.com>
2021-09-16 10:58:06 +0530

    redux devtool fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./reducers/index";
 
 const logger = createLogger();
 
 const store = createStore(
   reducers,
-  compose(
-    applyMiddleware(logger),
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(logger), applyMiddleware(thunk))
 );
 
 export default store;
